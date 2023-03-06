1bca2cbd62c03399f646cf9cada17e96126df796
Fatemeh Dashtizadeh <m.dashtizade@gmail.com>
2023-03-06 14:36:37 +0330

    Updated Js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
+function formatDate(timestamp){
+    let date = new Date(timestamp);
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let day = days[date.getDay()];
+    let hour = date.getHours();
+    if (hour < 10) {
+        hour = `0${hour}`;
+      }
+    let min = date.getMinutes();
+    if (min < 10) {
+        minu = `0${min}`;
+      }
+    return `${day} ${hour}:${min}`;
+}
+
 function displayTemperature(respone){
     console.log(respone.data);
     let temp = document.querySelector("#temperature");
@@ -9,7 +24,9 @@ function displayTemperature(respone){
     let humidity = document.querySelector("#humidity");
     humidity.innerHTML=respone.data.main.humidity;
     let wind = document.querySelector("#wind");
-    wind.innerHTML=Math.round(respone.data.wind.speed);  
+    wind.innerHTML=Math.round(respone.data.wind.speed);
+    let date = document.querySelector("#date");
+    date.innerHTML = formatDate(respone.data.dt * 1000);
 }
 
 let apiKey = "aca4dd3643b89e94dbd3cac6cf6f2638";
